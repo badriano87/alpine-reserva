799817f44cc4c0f9afa01424b02150113164dcd0
badriano87 <bruna.adriano@gmail.com>
2026-08-21 14:49:44 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,3 @@
+// Cole aqui a URL do seu Apps Script publicado como Web App.
+// Veja o passo a passo no arquivo GUIA-PASSO-A-PASSO.md, seção "Publicar o Apps Script".
+const APP_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwaVVtA_VhJC-PyrrRaNFVSt0PHbV3TZmHuBQ7f0DoJU-mQ0F6c5kGUwYfj1QEKrO_Yiw/exec';
